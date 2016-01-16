14f031f4e7d902f4befa47c059f6972f8e9cf1fa
Mehmet Ragıp Altuncu <mehmetaltuncu@gmail.com>
2016-01-15 20:58:41 -0800

    Table View test

--- TableViewPractice/ViewController.swift
+++ TableViewPractice/ViewController.swift
@@ -8,17 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    var nameArray = ["Mehmet","Clare","Kerim","Batuhan","Mevhibe","Hayrettin"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = nameArray[indexPath.row]
+        
+        return cell
+    }
+    
+    
+    
+
 
 
 }
